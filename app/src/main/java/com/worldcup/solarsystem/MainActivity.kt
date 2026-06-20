b556ac9cf150cda0818543358515512030adadb9
Ahmed Nasser <ahmedzaza857@gmail.com>
2026-06-20 13:38:04 +0300

    set light status bars appearance to false

--- app/src/main/java/com/worldcup/solarsystem/MainActivity.kt
+++ app/src/main/java/com/worldcup/solarsystem/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.InfiniteTransition
@@ -323,6 +324,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = false
         setContent { SolarSystemScreen() }
     }
 }
